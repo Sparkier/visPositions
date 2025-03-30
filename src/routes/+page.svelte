1e4133d8376fccbd61b90b8cd808e7975660d116
Alex Bäuerle <alex@a13x.io>
2025-03-30 15:08:21 -0700

    feat: make the main view mobile friendly

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,13 +4,12 @@
 	import PostFilters from '$lib/components/PostFilters.svelte';
 	import Input from '$lib/components/ui/Input.svelte';
 	import type { Post as PostType } from '$lib/types';
+	import Device from 'svelte-device-info';
 
 	let search = $state('');
 	let keywords = $state<string[]>([]);
 	let education = $state<string[]>([]);
 	let industry = $state<boolean | undefined>(undefined);
-
-	$inspect(keywords);
 
 	const filteredPosts = $derived(
 		page.data.posts.filter(
@@ -28,15 +27,17 @@
 <div class="flex h-full min-h-0 flex-col gap-2">
 	<Input name="search" placeholder="Search" bind:value={search} />
 	<div class="flex min-h-0 grow gap-2">
-		<div class="flex w-[300px] shrink-0 flex-col gap-2">
-			<PostFilters
-				bind:keywords
-				bind:education
-				bind:industry
-				posts={page.data.posts}
-				{filteredPosts}
-			/>
-		</div>
+		{#if !Device.isMobile}
+			<div class="flex w-[300px] shrink-0 flex-col gap-2">
+				<PostFilters
+					bind:keywords
+					bind:education
+					bind:industry
+					posts={page.data.posts}
+					{filteredPosts}
+				/>
+			</div>
+		{/if}
 		<div class="flex flex-col gap-2 overflow-y-auto">
 			{#each filteredPosts as post}
 				<Post {post} />
