e788a14cbaac6bc9f8c5a18577c89512ec5a6d5e
Alex Bäuerle <alex@a13x.io>
2025-05-10 14:37:46 -0700

    feat: allow vetting from right within the app

--- src/routes/private/post/+page.svelte
+++ src/routes/private/post/+page.svelte
@@ -6,6 +6,13 @@
 	import type { Post } from '$lib/types';
 
 	let editing: Post | undefined = undefined;
+
+	async function approvePost(postId: number) {
+		await fetch(`/api/post/${postId}/approve`, {
+			method: 'PATCH'
+		});
+		invalidateAll();
+	}
 </script>
 
 <div class="flex min-h-0 flex-col gap-6">
@@ -20,11 +27,18 @@
 		/>
 	</div>
 	<div class="flex min-h-0 flex-col gap-2">
-		<h1 class="border-b">Your Posts</h1>
+		<h1 class="border-b">{page.data.isAdmin ? 'Post Management' : 'Your Posts'}</h1>
 		<div class="flex flex-col gap-2 overflow-y-auto">
 			{#if page.data.posts.length}
 				{#each page.data.posts as post}
-					<PostView {post} deletable showVetted editClick={() => (editing = post)} />
+					<PostView
+						{post}
+						deletable
+						showVetted
+						editClick={() => (editing = post)}
+						isAdmin={page.data.isAdmin}
+						approveClick={() => approvePost(post.id)}
+					/>
 				{/each}
 			{:else}
 				<p>No posts found</p>
